00c92ab4a55a0125e854d961562744b2f438d907
Eelco van Vliet <eelcovv@gmail.com>
2025-04-15 21:18:18 +0200

    cleaning words

--- src/python_build_utils/clean_pyd_modules.py
+++ src/python_build_utils/clean_pyd_modules.py
@@ -1,9 +1,12 @@
 """ """
 
+import re
+from pathlib import Path
+
 import click
 
 from . import __version__
-from .collect_pyd_modules import collect_all_pyd_modules, get_venv_site_packages
+from .collect_pyd_modules import get_venv_site_packages
 
 
 @click.command(name="clean-pyd-modules", help="Clean all .pyd/.c build modules from a virtual environment.")
@@ -37,11 +40,29 @@ def clean_pyd_modules(venv_path: str | None = None, regex: str | None = None) ->
         click.echo("Could not locate site-packages in the specified environment.")
         return
 
-    click.echo(f"Collecting .pyd modules in '{venv_site_packages}'...")
-    pyd_sub_modules = collect_all_pyd_modules(venv_site_packages=venv_site_packages, regex=regex)
+    for extension in ["*.pyd", "*.c"]:
+        click.echo(f"Cleaning the {extension} files with '{regex}' filter in '{venv_site_packages}'...")
+        clean_by_extensions(venv_site_packages=venv_site_packages, regex=regex, extension=extension)
 
-    if not pyd_sub_modules:
-        click.echo("No .pyd modules found.")
-    else:
-        click.echo("Found the following .pyd submodules:")
-        click.echo("\n".join(f"- {module}" for module in pyd_sub_modules))
+
+def clean_by_extensions(venv_site_packages: Path, regex: str | None, extension: str) -> None:
+    file_candidates = list(venv_site_packages.rglob(extension))
+
+    if not file_candidates:
+        click.echo(f"No {extension} files found in {venv_site_packages}.")
+        return None
+
+    clean_any = False
+    for file_to_clean in file_candidates:
+        relative_path = file_to_clean.relative_to(venv_site_packages).as_posix()
+        if regex is not None and not re.search(regex, relative_path, re.IGNORECASE):
+            continue
+        click.echo(f"Removing {file_to_clean}")
+        try:
+            file_to_clean.unlink()
+        except Exception as e:
+            click.echo(f"Error removing {file_to_clean}: {e}", err=True)
+        else:
+            clean_any = True
+    if not clean_any:
+        click.echo(f"No {extension} files with '{regex}' filter found in {venv_site_packages}.")
